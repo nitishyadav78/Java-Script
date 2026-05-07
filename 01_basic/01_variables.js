6900be98cdde433b20a3c004b319c98f71983974
nitishyadav78 <107345152+nitishyadav78@users.noreply.github.com>
2026-05-07 10:34:59 +0000

    data type done

---
+++ 01_basic/01_variables.js
@@ -0,0 +1,15 @@
+const accountId = 49127
+let accountMail = "accout@gmail.com"
+var accountPassword = "92389578237"
+accountCity = "Mumbai"
+let accountState;
+
+// accountId = 8398
+/*var is not recommend to use because because
+of block scope and function scope */
+accountMail = "mumbai@gmail.com"
+accountPassword = "74296852985"
+accountCity = "Rampur"
+console.log(accountId);
+console.table([accountMail, accountId, accountPassword, accountCity, accountState])
+
---
+++ 01_basic/datatype.js
@@ -0,0 +1,18 @@
+"use strict"
+
+//alert(3+3)// this is not allowed here beacuse we use Node js not the browser
+
+console.log(3+5)
+console.log("Data Types")
+
+let name = "Java script"
+let age = 19
+let isFormFill = true
+//data types
+//number 2 to power 53
+//bigint
+//String = ""
+//boolean 
+//null // is a object object type
+//undefined // is undefined type
+console.table([typeof undefined, typeof null]);
