171bb1e7c54df498d48a16f15d71b288fb5e5bf3
nitishyadav78 <107345152+nitishyadav78@users.noreply.github.com>
2026-05-07 13:38:58 +0000

    DataType Done

---
+++ 01_basic/Conversiontypes.js
@@ -0,0 +1,28 @@
+ let number = ""
+//  console.log(typeof number)
+
+ let numberToString = Boolean(number)
+//  console.log(numberToString);
+
+//*******Operation */
+ let value = 4
+ let negValue = -value
+//  console.log(negValue);
+//Theser are operation
+//  console.log(2+2)
+//  console.log(2**3);
+//  console.log(12%5);
+
+let str1 = "Hello"
+let str2 = " Java Script"
+let str3 = str1 + str2
+// console.log(str3)
+ console.log(true)
+ console.log(+true);
+ console.log("1" + 2 + 4)
+ console.log(1+2+"3" + 0);
+ 
+ 
+ 
+ 
+  
--- 01_basic/datatype.js
+++ 01_basic/datatype.js
@@ -2,8 +2,8 @@
 
 //alert(3+3)// this is not allowed here beacuse we use Node js not the browser
 
-console.log(3+5)
-console.log("Data Types")
+// console.log(3+5)
+// console.log("Data Types")
 
 let name = "Java script"
 let age = 19
@@ -15,4 +15,47 @@ let isFormFill = true
 //boolean 
 //null // is a object object type
 //undefined // is undefined type
-console.table([typeof undefined, typeof null]);
+// console.table([typeof undefined, typeof null]);
+//***********Primitive Data Types and Non Primitives Data Types*******
+
+
+//Bigint, Number, float, long, double, String, Boolean
+
+//Non Primitive data Types
+//Object , Array , Function etc....
+const arr = [1,2,3,4,5,6,6]
+// console.log(arr)
+
+const id = Symbol("123")
+const id2 = Symbol("123")
+console.log(id === id2);
+
+// const Object = {eamilId, password}
+let MyObj = {
+    name : "Nitish",
+    email : "Ramesh@gmail.com",
+    age : 25
+
+}
+
+//************Memory **************************/
+//Stack(Primitive[Clone not exact value]) and Heap(Non Primitive [ Passing reference means excat value])
+let myName = "Nitish"
+let nameis = myName;
+nameis = "Yadav"
+// console.log(nameis);
+// console.log(myName);
+let userOne = {
+    email : "yadavnitihs@gmail.com",
+    mode : "upi"
+}
+let userTwo = userOne
+userTwo.email = "akash@gmail.com"
+userTwo.mode = "Cash"
+console.table([userTwo]);
+
+
+
+
+
+
